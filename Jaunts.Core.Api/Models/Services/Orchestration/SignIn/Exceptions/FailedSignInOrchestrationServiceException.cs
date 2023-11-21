28ce7b2d915ad6a37df0b2f1dec6c49a53f3d304
slimahmad6 <slimahmad6@outlook.com>
2023-11-21 13:38:53 +0100

    ORCHESTRATION: Created SignIn models

---
+++ Jaunts.Core.Api/Models/Services/Orchestration/SignIn/Exceptions/FailedSignInOrchestrationServiceException.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace Jaunts.Core.Api.Models.Services.Orchestration.SignIn.Exceptions
+{
+    public class FailedSignInOrchestrationServiceException : Xeption
+    {
+        public FailedSignInOrchestrationServiceException(Exception innerException)
+            : base(message: "Failed SignIn service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
---
+++ Jaunts.Core.Api/Models/Services/Orchestration/SignIn/Exceptions/SignInOrchestrationDependencyValidationException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace Jaunts.Core.Api.Models.Services.Orchestration.SignIn.Exceptions
+{
+    public class SignInOrchestrationDependencyValidationException : Xeption
+    {
+        public SignInOrchestrationDependencyValidationException(Xeption innerException)
+             : base(message: "SignIn dependency validation error occurred, please fix the errors and try again.",
+                  innerException)
+        { }
+    }
+}
