2a8aa418d4af5f4f33aeadcdfbe25c438dae9346
slimahmad6 <slimahmad6@outlook.com>
2023-11-21 14:56:43 +0100

    PROCESSINGS: Impplemented signIn processings

---
+++ Jaunts.Core.Api/Services/Processings/SignIn/SignInProcessingService.cs
@@ -0,0 +1,54 @@
+﻿using Jaunts.Core.Api.Brokers.Loggings;
+using Jaunts.Core.Api.Models.Services.Foundations.Users;
+using Jaunts.Core.Api.Services.Foundations.SignIn;
+
+namespace Jaunts.Core.Api.Services.Processings.SignIn
+{
+    public partial class SignInProcessingService : ISignInProcessingService
+    {
+      
+            private readonly ISignInService SignInService;
+            private readonly ILoggingBroker loggingBroker;
+
+            public SignInProcessingService(
+                ISignInService  SignInService,
+                ILoggingBroker loggingBroker
+
+                )
+            {
+                this.SignInService = SignInService;
+                this.loggingBroker = loggingBroker;
+
+            }
+
+
+        public ValueTask<bool> PasswordSignInAsync(
+            ApplicationUser user,string password,
+            bool isPersistent, bool lockoutOnFailure) =>
+        TryCatch(async () =>
+        {
+            ValidateUser(user);
+            ValidateString(password);
+           var response = await SignInService.PasswordSignInRequestAsync(user, password,isPersistent,lockoutOnFailure);
+           return response.Succeeded;
+        });
+
+        public ValueTask SignOutAsync() =>
+        TryCatch(async () =>
+        {
+            await SignInService.SignOutRequestAsync();
+
+        });
+
+        public ValueTask<bool> TwoFactorSignInAsync(
+            string provider, string code, 
+            bool isPersistent, bool rememberClient) =>
+        TryCatch(async () =>
+        {
+            ValidateString(provider);
+            ValidateString(code);
+            var response = await SignInService.TwoFactorSignInRequestAsync(provider, code,isPersistent,rememberClient);
+             return response.Succeeded;   
+        });
+    }
+}
