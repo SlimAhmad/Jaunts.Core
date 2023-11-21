91b494355ec271699cfa699785f04d9e3c707f6e
slimahmad6 <slimahmad6@outlook.com>
2023-11-21 14:45:46 +0100

    PROCESSINGS: Implemented SignIn Service

---
+++ Jaunts.Core.Api/Services/Processings/SignIn/ISignInProcessingService.cs
@@ -0,0 +1,12 @@
+﻿using Jaunts.Core.Api.Models.Services.Foundations.Users;
+using Jaunts.Core.Models.Email;
+
+namespace Jaunts.Core.Api.Services.Processings.SignIn
+{
+    public partial interface ISignInProcessingService
+    {
+        ValueTask SignOutAsync();
+        ValueTask<bool> PasswordSignInAsync(ApplicationUser user, string password, bool isPersistent, bool lockoutOnFailure);
+        ValueTask<bool> TwoFactorSignInAsync(string provider, string code, bool isPersistent, bool rememberClient);
+    }
+}
