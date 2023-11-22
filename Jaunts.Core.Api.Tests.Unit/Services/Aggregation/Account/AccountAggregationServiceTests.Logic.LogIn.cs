59cd8edb56511e002dad704d370dc20ba20cdd1e
slimahmad6 <slimahmad6@outlook.com>
2023-11-22 09:40:33 +0100

    ShouldLoginWithOtpAsync, ShouldLoginAsync -> PASS

---
+++ Jaunts.Core.Api.Tests.Unit/Services/Aggregation/Account/AccountAggregationServiceTests.Logic.LogIn.cs
@@ -0,0 +1,162 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Jaunts.Core.Api.Models.Auth;
+using Jaunts.Core.Api.Models.Services.Foundations.Role;
+using Jaunts.Core.Api.Models.Services.Foundations.Users;
+using Jaunts.Core.Models.Email;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jaunts.Core.Api.Tests.Unit.Services.Aggregation.Account
+{
+    public partial class AccountAggregationServiceTests
+    {
+        [Fact]
+        private async Task ShouldLoginAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            DateTimeOffset dateTime = randomDateTime;
+            List<string> randomRoleList = CreateRandomStringList();
+            LoginCredentialsApiRequest loginCredentialsApiRequest =
+               CreateLoginCredentialsApiRequest();
+            string password = GetRandomString();
+            string email = GetRandomString();
+
+
+            ApplicationUser randomUser =
+                    CreateRandomUser(dateTime);
+
+            IQueryable<ApplicationRole> randomRoles =
+                CreateRandomRoles(dateTime, randomRoleList);
+
+            ApplicationUser inputUser = randomUser;
+            ApplicationUser storageUser = inputUser;
+        
+            UserAccountDetailsApiResponse randomUserProfileDetails =
+                CreateUserAccountDetailsApiResponse(storageUser);
+
+            UserAccountDetailsApiResponse expectedUserProfileDetails =
+                    randomUserProfileDetails;
+
+            this.userOrchestrationMock.Setup(broker =>
+                broker.RetrieveUserByEmailOrUserNameAsync(loginCredentialsApiRequest))
+                    .ReturnsAsync(storageUser);
+
+            this.userOrchestrationMock.Setup(broker =>
+                broker.CheckPasswordValidityAsync(password,storageUser.Id))
+                    .ReturnsAsync(storageUser);
+
+            this.jwtOrchestrationMock.Setup(broker =>
+                broker.JwtAccountDetailsAsync(It.IsAny<ApplicationUser>()))
+                        .ReturnsAsync(randomUserProfileDetails);
+
+            // when
+            UserAccountDetailsApiResponse actualAuth =
+                await this.accountAggregationService.LogInRequestAsync(loginCredentialsApiRequest);
+
+            // then
+            actualAuth.Should().BeEquivalentTo(expectedUserProfileDetails);
+
+            this.userOrchestrationMock.Verify(broker =>
+                broker.RetrieveUserByEmailOrUserNameAsync(It.IsAny<LoginCredentialsApiRequest>()),
+                    Times.Once);
+
+            this.userOrchestrationMock.Verify(broker =>
+                 broker.CheckPasswordValidityAsync(It.IsAny<string>(), It.IsAny<Guid>()),
+                       Times.Once);
+
+            this.jwtOrchestrationMock.Verify(broker =>
+                  broker.JwtAccountDetailsAsync(It.IsAny<ApplicationUser>()),
+                        Times.Once);
+
+ 
+            this.userOrchestrationMock.VerifyNoOtherCalls();
+            this.jwtOrchestrationMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task ShouldLoginWithOtpAsync() 
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            DateTimeOffset dateTime = randomDateTime;
+            List<string> randomRoleList = CreateRandomStringList();
+            LoginCredentialsApiRequest loginCredentialsApiRequest =
+               CreateLoginCredentialsApiRequest();
+            SendEmailResponse sendEmailResponse = CreateSendEmailResponse();
+            string password = GetRandomString();
+            string email = GetRandomString();
+            bool randomBoolean = GetRandomBoolean();
+
+
+            ApplicationUser randomUser =
+                    CreateRandomUser(dateTime);
+
+            IQueryable<ApplicationRole> randomRoles =
+                CreateRandomRoles(dateTime, randomRoleList);
+
+            ApplicationUser inputUser = randomUser;
+            ApplicationUser storageUser = inputUser;
+            storageUser.TwoFactorEnabled = true;
+
+            UserAccountDetailsApiResponse inputUserProfileDetails =
+                CreateUserAccountDetailsApiResponse(storageUser);
+
+            UserAccountDetailsApiResponse expectedUserProfileDetails =
+                    inputUserProfileDetails;
+
+            this.userOrchestrationMock.Setup(broker =>
+             broker.RetrieveUserByEmailOrUserNameAsync(loginCredentialsApiRequest))
+                 .ReturnsAsync(storageUser);
+
+            this.signInOrchestrationMock.Setup(broker =>
+                broker.SignOutAsync());
+
+            this.signInOrchestrationMock.Setup(broker =>
+               broker.PasswordSignInAsync(storageUser, password,randomBoolean,randomBoolean))
+                   .ReturnsAsync(randomBoolean);
+
+            this.emailOrchestrationBrokerMock.Setup(broker =>
+                broker.TwoFactorMailAsync(storageUser))
+                    .ReturnsAsync(inputUserProfileDetails);
+
+            // when
+            UserAccountDetailsApiResponse actualAuth =
+                await this.accountAggregationService.LogInRequestAsync(loginCredentialsApiRequest);
+
+            // then
+            actualAuth.Should().BeEquivalentTo(expectedUserProfileDetails);
+
+            this.userOrchestrationMock.Verify(broker =>
+                broker.RetrieveUserByEmailOrUserNameAsync(It.IsAny<LoginCredentialsApiRequest>()),
+                    Times.Once);
+
+            this.signInOrchestrationMock.Verify(broker =>
+             broker.SignOutAsync(),
+                 Times.Once);
+
+            this.signInOrchestrationMock.Verify(broker =>
+             broker.PasswordSignInAsync(It.IsAny<ApplicationUser>(),It.IsAny<string>(),It.IsAny<bool>(),It.IsAny<bool>()),
+                 Times.Once);
+
+            this.emailOrchestrationBrokerMock.Verify(broker =>
+                 broker.TwoFactorMailAsync(It.IsAny<ApplicationUser>()),
+                       Times.Once);
+
+            this.userOrchestrationMock.VerifyNoOtherCalls();
+            this.signInOrchestrationMock.VerifyNoOtherCalls();
+            this.emailOrchestrationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
