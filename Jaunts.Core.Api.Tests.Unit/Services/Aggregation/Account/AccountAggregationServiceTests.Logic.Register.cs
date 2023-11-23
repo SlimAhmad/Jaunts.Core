500c13539d9fbc2231f392315824e954e3f96b5d
slimahmad6 <slimahmad6@outlook.com>
2023-11-23 12:33:31 +0100

    ShouldRegisterUserAsync -> PASS

---
+++ Jaunts.Core.Api.Tests.Unit/Services/Aggregation/Account/AccountAggregationServiceTests.Logic.Register.cs
@@ -0,0 +1,95 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Jaunts.Core.Api.Models.Auth;
+using Jaunts.Core.Api.Models.Services.Foundations.Role;
+using Jaunts.Core.Api.Models.Services.Foundations.Users;
+using Jaunts.Core.Models.Email;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jaunts.Core.Api.Tests.Unit.Services.Aggregation.Account
+{
+    public partial class AccountAggregationServiceTests
+    {
+        [Fact]
+        private async Task ShouldRegisterUserAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            DateTimeOffset dateTime = randomDateTime;
+            List<string> randomRoleList = CreateRandomStringList();
+            RegisterUserApiRequest registerUserApiRequest =
+               CreateRegisterUserApiRequest(dateTime);
+            ApplicationUser randomUser = 
+                ConvertToUserRequest(registerUserApiRequest);
+            ApplicationUser inputUser = randomUser;
+            ApplicationUser storageUser = inputUser.DeepClone();
+            SendEmailResponse sendEmailResponse = CreateSendEmailResponse();
+            string password = GetRandomString();
+
+            IQueryable<ApplicationRole> randomRoles =
+                CreateRandomRoles(dateTime, randomRoleList);
+        
+            UserAccountDetailsApiResponse randomUserProfileDetails =
+                CreateRegisterUserResponse(registerUserApiRequest);
+
+            UserAccountDetailsApiResponse expectedUserProfileDetails =
+                    randomUserProfileDetails;
+
+            this.userOrchestrationMock.Setup(orchestration =>
+                orchestration.RegisterUserAsync(It.IsAny<ApplicationUser>(),It.IsAny<string>()))
+                    .ReturnsAsync(storageUser);
+
+            this.userOrchestrationMock.Setup(orchestration =>
+                orchestration.AddUserToRoleAsync(It.IsAny<ApplicationUser>(),It.IsAny<string>()))
+                    .ReturnsAsync(storageUser);
+
+            this.emailOrchestrationMock.Setup(orchestration =>
+                 orchestration.VerificationMailAsync(It.IsAny<ApplicationUser>()))
+                     .ReturnsAsync(sendEmailResponse);
+
+            this.jwtOrchestrationMock.Setup(orchestration =>
+                orchestration.JwtAccountDetailsAsync(It.IsAny<ApplicationUser>()))
+                        .ReturnsAsync(randomUserProfileDetails);
+
+            // when
+            UserAccountDetailsApiResponse actualAuth =
+                await this.accountAggregationService.RegisterUserRequestAsync(registerUserApiRequest);
+
+            // then
+            actualAuth.Should().BeEquivalentTo(expectedUserProfileDetails);
+
+            this.userOrchestrationMock.Verify(orchestration =>
+                orchestration.RegisterUserAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()),
+                    Times.Once);
+
+            this.userOrchestrationMock.Verify(orchestration =>
+                 orchestration.AddUserToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()),
+                       Times.Once);
+
+            this.emailOrchestrationMock.Verify(orchestration =>
+             orchestration.VerificationMailAsync(It.IsAny<ApplicationUser>()),
+                   Times.Once);
+
+            this.jwtOrchestrationMock.Verify(orchestration =>
+                  orchestration.JwtAccountDetailsAsync(It.IsAny<ApplicationUser>()),
+                        Times.Once);
+         
+            this.userOrchestrationMock.VerifyNoOtherCalls();
+            this.emailOrchestrationMock.VerifyNoOtherCalls();
+            this.jwtOrchestrationMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+     
+    }
+}
