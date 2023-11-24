df598ea009bfa342b0dc33868efb6d4d8e27b4c4
slimahmad6 <slimahmad6@outlook.com>
2023-11-24 12:03:15 +0100

    CODE RUB: Changed naming

---
+++ Jaunts.Core.Api/Models/Services/Foundations/Auth/Exceptions/AlreadyExistsAccountException.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace Jaunts.Core.Api.Models.Services.Foundations.Auth.Exceptions
+{
+    public class AlreadyExistsAccountException : Xeption
+    {
+        public AlreadyExistsAccountException(Exception innerException)
+            : base(message: "Auth with the same id already exists.", innerException)
+        { }
+
+        public AlreadyExistsAccountException(string message,Exception innerException)
+            : base(message) { }
+    }
+}
