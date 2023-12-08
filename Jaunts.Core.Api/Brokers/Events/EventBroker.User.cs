e30120f4323d5d175a06d9fd7eb583d432ba8bfe
slimahmad6 <slimahmad6@outlook.com>
2023-12-08 15:56:15 +0100

    Brokers: Event Broker

---
+++ Jaunts.Core.Api/Brokers/Events/EventBroker.User.cs
@@ -0,0 +1,15 @@
+﻿using Jaunts.Core.Api.Models.Services.Foundations.Users;
+
+namespace Jaunts.Core.Api.Brokers.Events
+{
+    public partial class EventBroker
+    {
+        private static Func<ApplicationUser, ValueTask<ApplicationUser>> UserEventHandler;
+
+        public void ListenToUserEvent(Func<ApplicationUser, ValueTask<ApplicationUser>> userEventHandler) =>
+            UserEventHandler = userEventHandler;
+
+        public async ValueTask PublishUserEventAsync(ApplicationUser user) =>
+            await UserEventHandler(user);
+    }
+}
---
+++ Jaunts.Core.Api/Brokers/Events/EventBroker.cs
@@ -0,0 +1,9 @@
+﻿using Jaunts.Core.Api.Models.Services.Foundations.Users;
+
+namespace Jaunts.Core.Api.Brokers.Events
+{
+    public partial class EventBroker : IEventBroker
+    {
+       
+    }
+}
